04cc712fd2f0bae0f4b0dd127b85d42359c8c564
Semih TOLGA <109283539+semihtolga@users.noreply.github.com>
2022-10-03 22:25:42 +0300

    channges

--- javascript/class-notes/js23-testing/src/math.test.js
+++ javascript/class-notes/js23-testing/src/math.test.js
@@ -18,17 +18,45 @@ describe("calculateTotal", () => {
     expect(calculateTotal).toBeDefined();
   });
 
-  it("should return a number", () => {});
+  it("should return a number", () => {
+    expect(calculateTotal(100, 0.2)).toBe(120);
+  });
+
+  it("should return with default VAT", () => {
+    expect(calculateTotal(200)).toBe(236);
+  });
 });
 
 describe("celsiusToFahrenheit", () => {
-  //
+  it("should exist", () => {
+    expect(celsiusToFahrenheit).toBeDefined();
+  });
+
+  it("should return a number", () => {
+    expect(celsiusToFahrenheit(10)).toBe(50);
+  });
+
+  it("should convert 20 C to 68 F", () => {
+    expect(celsiusToFahrenheit(20)).toBe(68);
+  });
 });
 
 describe("fahrenheitToCelsius", () => {
-  //
+  it("should exist", () => {
+    expect(fahrenheitToCelsius).toBeDefined();
+  });
+
+  it("should convert 32F to 0C", () => {
+    expect(fahrenheitToCelsius(32)).toBe(0);
+  });
 });
 
 describe("fibonacci", () => {
-  //
+  it("should exist", () => {
+    expect(fibonacci).toBeDefined();
+  });
+
+  it("should return a number", () => {
+    expect(fibonacci(1)).toBe(1);
+  });
 });
